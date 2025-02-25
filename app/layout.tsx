5b5a8132d93480f2489dde8e058e41a5d2bcf25c
Tomás Di Bacco <100334543+Tdibacco17@users.noreply.github.com>
2025-02-25 17:15:48 -0300

    meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,21 +39,21 @@ export default function RootLayout({
           strategy="afterInteractive"
         >
           {`
-                !function(f,b,e,v,n,t,s)
-                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t,s)}(window, document,'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '308659638359795');
-                fbq('track', 'PageView');
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1167956061337350');
+          fbq('track', 'PageView');
                 `}
         </Script>
         <noscript>
           <Image height="1" width="1" style={{ display: "none" }} alt='fbqPixel'
-            src={"https://www.facebook.com/tr?id=308659638359795&ev=PageView&noscript=1"}
+            src={"https://www.facebook.com/tr?id=1167956061337350&ev=PageView&noscript=1"}
           />
         </noscript>
       </head>
